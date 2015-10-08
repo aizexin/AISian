e4ff6d5f6f0b719dcd0cdb48b5aba509e19d5497
aizexin <443792431@qq.com>
2015-10-09 00:21:01 +0800

    完成下拉刷新和消息数量提示

--- AISian/Class/Home(首页)/Controller/AiHomeViewController.m
+++ AISian/Class/Home(首页)/Controller/AiHomeViewController.m
@@ -15,7 +15,7 @@
 #import "AIDefine.h"
 #import "UIView+Extension.h"
 #import "AIPopMenu.h"
-#import "AFHTTPSessionManager.h"
+#import "AFHTTPRequestOperationManager.h"
 #import "AIAccountTool.h"
 #import "AIAccountModel.h"
 #import "MJExtension.h"
@@ -31,34 +31,130 @@
 @implementation AiHomeViewController
 
 
+-(NSMutableArray *)statuses{
+    if (!_statuses) {
+        _statuses = [NSMutableArray array];
+    }
+    return _statuses;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     AILog(@"%@",NSHomeDirectory());
     [self setupNavBar];
-    //加载数据
-    [self loadNewData];
-    AILog(@"");
+//    //加载数据
+//    [self loadNewData];
+    //集成刷新控件
+    [self setupRefresh];
+}
+
+/**
+ *  集成刷新控件
+ */
+- (void)setupRefresh{
+    UIRefreshControl *refresh = [[UIRefreshControl alloc]init];
+    [self.tableView addSubview:refresh];
+    //2.监听refresh
+    [refresh addTarget:self action:@selector(refreshControlStateChange:) forControlEvents:(UIControlEventValueChanged)];
 }
 /**
- *  加载数据
+ *  刷新方法
  */
-- (void)loadNewData{
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+-(void)refreshControlStateChange:(UIRefreshControl*)refresh{
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     //从沙盒中获得accessToken
+    //封装参数
     AIAccountModel *account = [AIAccountTool account];
     params[@"access_token"] = account.access_token;
+    AIStatusesModel *statuse = [self.statuses firstObject];
+    if (statuse) {
+        params[@"since_id"] = statuse.idstr;
+    }
     //get请求
-    [manager GET:@"https://api.weibo.com/2/statuses/home_timeline.json" parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
+    [manager GET:@"https://api.weibo.com/2/statuses/home_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
         AILog(@"请求成功");
         NSArray *statuses = responseObject[@"statuses"];
-        self.statuses = [AIStatusesModel objectArrayWithKeyValuesArray:statuses];
+        AILog(@"%ld",statuses.count);
+        statuses = [AIStatusesModel objectArrayWithKeyValuesArray:statuses];
+        //讲数据查到最前面
+        NSIndexSet *indexSet = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, statuses.count)];
+        [self.statuses insertObjects:statuses atIndexes:indexSet];
         [self.tableView reloadData];
-    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        //提示用户刷新数量
+        [self showNewStatuesCount:statuses.count];
+        //停止刷新
+        [refresh endRefreshing];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         AILog(@"请求失败%@",error.description);
+        //停止刷新
+        [refresh endRefreshing];
     }];
 }
+/**
+ *  提示用户刷新数量
+ *
+ *  @param count 最新微博的数量
+ */
+-(void)showNewStatuesCount:(NSInteger)count{
+    UILabel *label = [[UILabel alloc]init];
+    if (count) {
+        label.text = [NSString stringWithFormat:@"共有%ld条新数据",count];
+    }else{
+        label.text = @"没有新数据";
+    }
+    //3.设置背景
+    label.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"timeline_new_status_background"]];
+    [label setTextAlignment:(NSTextAlignmentCenter)];
+    label.textColor = [UIColor whiteColor];
+    //设置frame
+    label.width = self.view.width;
+    label.height = 35;
+    label.x = 0;
+    label.y = 64 - label.height;
+    
+    //添加到当行控制器的view中
+    [self.navigationController.view insertSubview:label belowSubview:self.navigationController.navigationBar];
+    //动画
+    CGFloat duration = 0.75;
+    label.alpha = 0.;
+    //往下移动
+    [UIView animateWithDuration:duration animations:^{
+        [label setTransform:(CGAffineTransformMakeTranslation(0, label.height))];
+        label.alpha = 1.;
+    }];
+    //往上移动
+    [UIView animateWithDuration:duration delay:1. options:(UIViewAnimationOptionCurveEaseOut) animations:^{
+        label.transform = CGAffineTransformIdentity;
+        label.alpha = 0.;
+    } completion:^(BOOL finished) {
+        [label removeFromSuperview];
+    }];
+    
+}
+///**
+// *  加载数据
+// */
+//- (void)loadNewData{
+//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
+//    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+//    //从沙盒中获得accessToken
+//    AIAccountModel *account = [AIAccountTool account];
+//    params[@"access_token"] = account.access_token;
+//    //get请求
+//
+//    [manager GET:@"https://api.weibo.com/2/statuses/home_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        AILog(@"请求成功");
+//        NSArray *statuses = responseObject[@"statuses"];
+//        self.statuses = [AIStatusesModel objectArrayWithKeyValuesArray:statuses];
+//        [self.tableView reloadData];
+//        
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        AILog(@"请求失败%@",error.description);
+//    }];
+//}
 /**
  *  设置navBar
  */
