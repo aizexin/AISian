887447d918ef33e6b1adf1c345b7676ff886b428
aizexin <443792431@qq.com>
2015-10-08 08:06:54 +0800

    准备获取accesstoken

--- AISian/Class/OAuth(授权)/controller/AIOAuthViewController.m
+++ AISian/Class/OAuth(授权)/controller/AIOAuthViewController.m
@@ -27,6 +27,28 @@
     webView.delegate = self;
 }
 
-
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    AILog(@"%@",request.URL.absoluteString);
+    //1.获得请求路径
+    NSString *url = request.URL.absoluteString;
+    //2.判断是不是回调地址
+    NSRange range = [url rangeOfString:@"http://www.baidu.com/?code="];
+    if (range.location != NSNotFound) {//是回调地址
+//        获得code
+        NSString *code = [[url componentsSeparatedByString:@"http://www.baidu.com/?code="]lastObject];
+#warning  根据code获得accessToken
+//        [self access]
+    }
+    return YES;
+}
+- (void)webViewDidStartLoad:(UIWebView *)webView{
+    
+}
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    
+}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(nullable NSError *)error{
+    
+}
 
 @end
